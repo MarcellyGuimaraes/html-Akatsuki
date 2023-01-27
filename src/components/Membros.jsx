31cdfa1163cca2f450f4c3a3ee97fabfbc7d9803
MarcellyGuimaraes <souzaguimaraesmarcelly@gmail.com>
2023-01-27 16:31:42 -0300

    feat: arrumando o css da área 'membros' da home

--- src/components/Membros.jsx
+++ src/components/Membros.jsx
@@ -1,40 +1,22 @@
 import React from 'react'
+import { membros } from '../assets/data'
+import gifAkatsuki from '../assets/img/Akatsuki.png.webp'
 
 const Membros = () => {
   return (
-    <section className="renegados">
-      <h3 className="principal-title">Membros</h3>
-      <div className="conteudo-renegados">
-        <ul className="lista-renegados">
-          <li className="itens">Pain</li>
-          <li className="itens">Hidan</li>
-          <li className="itens">Itachi</li>
-          <li className="itens">Zetzu</li>
-          <li className="itens">Konan</li>
-          <li className="itens">Deidara</li>
-          <li className="itens">Orochimaru</li>
-          <li className="itens">Kakuzu</li>
-          <li className="itens">Kisame</li>
-          <li className="itens">Sasori</li>
-          <li className="itens">Obito</li>
+    <section className="p-10 bg-red-900 text-white flex flex-col ">
+      <h3 className="text-2xl font-bold">Membros</h3>
+      <div className="w-84 flex">
+        <ul className="w-2/5">
+          {membros.map((membro) => (
+            <li className="itens">⧽ {membro.name}</li>
+          ))}
         </ul>
         <img
           id="akatsuki"
-          src="img/Akatsuki.png.webp"
-          className="imagem-renegados"
+          src={gifAkatsuki}
+          className="w-[60%] md:w-3/5 bg-white"
         />
-      </div>
-
-      <div className="video">
-        <iframe
-          width="100%"
-          height="315"
-          src="https://www.youtube.com/embed/-oYMo8k22Vw"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
       </div>
     </section>
   )
