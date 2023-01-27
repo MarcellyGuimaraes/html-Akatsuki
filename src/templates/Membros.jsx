1c21ad4293257ba0240aaf6c04d24c82e2280c7d
MarcellyGuimaraes <souzaguimaraesmarcelly@gmail.com>
2023-01-27 17:08:45 -0300

    feat: arrumando página de membros

--- src/templates/Membros.jsx
+++ src/templates/Membros.jsx
@@ -1,85 +1,19 @@
 import React from 'react'
+import { membros } from '../assets/data'
 
 const Membros = () => {
   return (
-    <main>
-      <ul class="membros">
-        <li>
-          <h2>Hidan</h2>
-          <img src="img/hidan.jpg" class="membro" />
-          <p class="membros-jutsu">Hidan</p>
-          <p class="membros-name">(飛段)</p>
-        </li>
-
-        <li>
-          <h2>Itachi</h2>
-          <img src="img/Itachi.png" class="membro" />
-          <p class="membros-jutsu">Itachi</p>
-          <p class="membros-name">(うちはイタチ)</p>
-        </li>
-
-        <li>
-          <h2>Pain</h2>
-          <img src="img/pain.jpg" class="membro" />
-          <p class="membros-jutsu">Pain</p>
-          <p class="membros-name">(ペイン)</p>
-        </li>
-
-        <li>
-          <h2>Zetzu</h2>
-          <img src="img/zetsu.jpg" class="membro" />
-          <p class="membros-jutsu">Zetsu</p>
-          <p class="membros-name">(白ゼツ)</p>
-        </li>
-
-        <li>
-          <h2>Konan</h2>
-          <img src="img/Konan.png" class="membro" />
-          <p class="membros-jutsu">Konan</p>
-          <p class="membros-name">(小南)</p>
-        </li>
-
-        <li>
-          <h2>Deidara</h2>
-          <img src="img/deidara.jpg" class="membro" />
-          <p class="membros-jutsu">Deidara</p>
-          <p class="membros-name">(デイダラ)</p>
-        </li>
-
-        <li>
-          <h2>Orochimaru</h2>
-          <img src="img/Orochimaru.jpg" class="membro" />
-          <p class="membros-jutsu">Orochimaru</p>
-          <p class="membros-name">(大蛇丸)</p>
-        </li>
-
-        <li>
-          <h2>Kakuzu</h2>
-          <img src="img/Kakuzu.jpg" class="membro" />
-          <p class="membros-jutsu">Kakuzu</p>
-          <p class="membros-name">(角都)</p>
-        </li>
-
-        <li>
-          <h2>Kisame</h2>
-          <img src="img/Kisame.png" class="membro" />
-          <p class="membros-jutsu">Kisame</p>
-          <p class="membros-name">(干柿鬼鮫)</p>
-        </li>
-
-        <li>
-          <h2>Sasori</h2>
-          <img src="img/Sasori.png" class="membro" />
-          <p class="membros-jutsu">Sasori</p>
-          <p class="membros-name">(サソリ)</p>
-        </li>
-
-        <li>
-          <h2>Obito</h2>
-          <img src="img/obito.jpg" class="membro" />
-          <p class="membros-jutsu">Obito</p>
-          <p class="membros-name">(うちはオビト)</p>
-        </li>
+    <main className="font-mono flex flex-col items-center bg-slate-200 text-slate-800">
+      <h1 className="text-3xl font-bold mt-10">Membros</h1>
+      <ul class="grid grid-cols-1 w-4/5 md:grid-cols-3 place-items-center">
+        {membros.map((membro) => (
+          <li className="py-16 text-center p-3 my-2">
+            <h2 className="text-3xl font-black">{membro.name}</h2>
+            <img src={membro.urlImg} class="h-72 w-60 rounded-2xl" />
+            <p class="text-xl font-bold">{membro.name}</p>
+            <p class="font-bold">({membro.japanName})</p>
+          </li>
+        ))}
       </ul>
     </main>
   )
